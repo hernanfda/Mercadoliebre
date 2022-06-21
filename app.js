39e32a1e76eac87ca78ba8f6d336bc89de424c8a
hernanfda <hernanfda@hotmail.com>
2022-06-21 20:29:06 -0300

    Prueba

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 4000;;
+const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || 'localhost';
 
 const publicPath = path.resolve(__dirname, "./public");
